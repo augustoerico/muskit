b2710a870d2daf337af72d673f2ce387f69e2f4d
Erico Silva <augusto.ericosilva@gmail.com>
2024-03-31 04:29:05 -0300

    Add monkeypatch to change the cwd during test

--- QuantumSoftwareTestingTools/Muskit/Muskit/test_main.py
+++ QuantumSoftwareTestingTools/Muskit/Muskit/test_main.py
@@ -1,11 +1,19 @@
 """
 Main app tests
 """
+import pytest
 from typer.testing import CliRunner
 
 from main import app
 
 runner = CliRunner()
+
+@pytest.fixture(autouse=True)
+def change_test_dir(request, monkeypatch):
+    """
+    changes the working dir to this one
+    """
+    monkeypatch.chdir(request.fspath.dirname)
 
 def test_should_create_mutants_with_default_inputs():
     """
@@ -16,7 +24,7 @@ def test_should_create_mutants_with_default_inputs():
     args = ["create",
             "--config", "configs/default.create.toml",
             "--circuit", "../Example/QRAM_program.py"]
-    
+
     # when
     result = runner.invoke(app, args)
 
