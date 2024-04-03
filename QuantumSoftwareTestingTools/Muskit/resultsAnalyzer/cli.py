6d8c0a9997601853bacf2e3a93ee11445931f9d8
Erico Silva <augusto.ericosilva@gmail.com>
2024-04-03 05:57:13 -0300

    Add Quantum Program specification parser (#10)
    * Add spec parser to CLI
    
    * Fix file extension on parsed results JSON
    
    * Ignore generated spec from test

--- QuantumSoftwareTestingTools/Muskit/resultsAnalyzer/cli.py
+++ QuantumSoftwareTestingTools/Muskit/resultsAnalyzer/cli.py
@@ -7,19 +7,47 @@ import typer
 from typing_extensions import Annotated
 from rich.console import Console
 
-from results_parser import parse
+from results_parser import parse as r_parse
+from specification_parser import parse as s_parse
 
 app = typer.Typer()
 
 typer_options = { "exists": True, "readable": True, "resolve_path": True }
-error_console = Console(stderr=True)
+fail_console = Console(stderr=True)
+pass_console = Console()
+
 
 @app.command()
-def parse_results(results_file_path: Annotated[Path, typer.Option(**typer_options)]):
+def parse_results(results: Annotated[Path, typer.Option(**typer_options)]):
     """
     parse results from Muskit.execute into multiple json files
     """
-    parse(results_file_path)
+    r_parse(results)
+
+@app.command()
+def parse_spec(spec: Annotated[Path, typer.Option(**typer_options)]):
+    """
+    parses a specification into JSON format
+    """
+    s_parse(spec)
+
+# @app.command()
+# def verify_woo(
+#     results: Annotated[Path, typer.Option(**typer_options)],
+#     specification: Annotated[Path, typer.Option(**typer_options)]
+#     ):
+#     """
+#     Verify results obtained from Muskit.execute against the program specification.
+#         It also creates JSON files for each mutant execution
+#     """
+#     try:
+#         # assert_all_valid_outputs()
+#         message = "PASS"
+#         pass_console.print(message, style="green")
+#     except AssertionError as _:
+#         message = "FAIL"
+#         fail_console.print(message, style="red")
+
 
 if __name__ == '__main__':
     app()
---
+++ QuantumSoftwareTestingTools/Muskit/resultsAnalyzer/specification_parser.py
@@ -0,0 +1,101 @@
+"""
+Module to parse the Quantum Program specification into a dictionary
+"""
+from os.path import dirname, basename
+from pathlib import Path
+from typing import List, Tuple
+
+import simplejson
+
+def parse(specification_file_path: Path, save_output: bool = True):
+    """
+    parses the Quantum Program specification file into a dictionary
+    """
+    output_probabilities_by_input = {}
+    with open(specification_file_path, 'r', encoding='utf-8') as file:
+        for line in file.readlines():
+            input_value, output, probability = \
+                parse_specification_line(line)
+            output_probabilities_by_input = \
+                add_to_output_probabilities_by_input_dict(
+                    input_value, output, probability,
+                    output_probabilities_by_input
+                    )
+
+    parsed = fix_number_of_bits(output_probabilities_by_input)
+    if save_output:
+        save_parsed_spec(specification_file_path, parsed)
+    return parsed
+
+def save_parsed_spec(specification_file_path: Path, output_probabilities_by_input: dict):
+    """
+    saves the output_probabilities_by_input dict into a JSON object
+    """
+    dir_name = dirname(specification_file_path)
+    file_name = basename(specification_file_path).split('.', 1)[0]
+    json_file_path = f"{dir_name}/{file_name}.spec.json"
+    with open(json_file_path, 'w', encoding='utf-8') as file:
+        simplejson.dump(output_probabilities_by_input, fp=file, indent=4)
+
+def fix_number_of_bits(output_probabilities_by_input: dict):
+    """
+    add zeros to the left to match the highest bit counts
+    """
+    max_lenght_of_bits = 0
+    for i in output_probabilities_by_input.keys():
+        lenght_of_bits = len(i)
+        if lenght_of_bits > max_lenght_of_bits:
+            max_lenght_of_bits = lenght_of_bits
+    result = {}
+    for i, op in output_probabilities_by_input.items():
+        result[i.zfill(max_lenght_of_bits)] = {
+            o.zfill(max_lenght_of_bits): p
+            for o, p in op.items()
+        }
+    return result
+
+def parse_specification_line(line: str) -> Tuple[str, str, float]:
+    """
+    parse the Quantum Program specification file line into a tuple
+        input, output, probability
+    """
+    return *extract_input_output_pair(line), extract_probability(line)
+
+def extract_input_output_pair(line: str) -> List[str]:
+    """
+    extracts the (input, output) pair from the specification line
+    """
+    input_output_pair = line[
+        line.find('(') + 1:line.find(')')
+        ]
+    input_str, output_str = input_output_pair.split(',')
+    input_value = int(input_str.strip())
+    output = int(output_str.strip())
+    return [format(input_value, 'b'), format(output, 'b')]
+
+def extract_probability(line: str) -> float:
+    """
+    extracts the probability from the specification line
+    """
+    probability_str = line.split(':')[1]
+    return float(probability_str.strip())
+
+def add_to_output_probabilities_by_input_dict(
+        input_value: str, output: str, probability: float,
+        counts_by_input: dict) -> dict:
+    """
+    adds outputs and respective probabilities into the
+        output_probabilities_by_input dictionary
+    """
+    if input_value in counts_by_input:
+        # concatenate dictionaries
+        counts_by_input[input_value] = {
+            **counts_by_input[input_value],
+            output: probability
+        }
+    else:
+        # create entry in dictionary
+        counts_by_input[input_value] = {
+            output: probability
+        }
+    return counts_by_input
